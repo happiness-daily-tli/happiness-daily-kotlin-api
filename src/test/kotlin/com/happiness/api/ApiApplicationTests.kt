fdcf7d5712e6c2d778273b5d17e7127032714eb4
Connor Kim <k.connor614@gmail.com>
2021-08-04 20:45:30 +0900

    init api

---
+++ src/test/kotlin/com/happiness/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.happiness.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
