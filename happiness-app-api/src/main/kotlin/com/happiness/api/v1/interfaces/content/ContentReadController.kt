50eb57416e6a642004376c08a3091165e344e120
14km <k.connor614@gmail.com>
2021-12-08 23:48:00 +0900

    Generate, Read Controller 생성

--- happiness-app-api/src/main/kotlin/com/happiness/api/v1/interfaces/content/ContentReadController.kt
+++ happiness-app-api/src/main/kotlin/com/happiness/api/v1/interfaces/content/ContentReadController.kt
@@ -1,12 +1,13 @@
 package com.happiness.api.v1.interfaces.content
 
-import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ContentGenerateController {
-    @PostMapping("v1/content")
-    fun generate(): String {
+class ContentReadController {
+
+    @GetMapping("v1/content")
+    fun read(): String {
         return "1"
     }
 }
