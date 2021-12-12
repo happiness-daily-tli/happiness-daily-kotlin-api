4967cafaf233e499eed3bba6d7a2dc6c80cb3472
14km <k.connor614@gmail.com>
2021-12-12 20:05:03 +0900

    adapter 구조 추가

--- happiness-app-api/src/main/kotlin/com/happiness/api/v1/adapter/in/presentation/ContentGenerateController.kt
+++ happiness-app-api/src/main/kotlin/com/happiness/api/v1/adapter/in/presentation/ContentGenerateController.kt
@@ -1,18 +1,27 @@
 package com.happiness.api.v1.adapter.`in`.presentation
 
 import com.happiness.api.v1.application.dto.content.ContentGenerateRequestDto
+import com.happiness.api.v1.application.port.`in`.ContentGenerateUseCase
+import com.happiness.api.v1.application.port.command.ContentGenerateCommand
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
 @RestController
-class ContentGenerateController {
+class ContentGenerateController(
+    private val contentGenerateUseCase: ContentGenerateUseCase
+) {
 
     @PostMapping("v1/content")
     fun generate(
         @RequestBody @Valid contentGenerateRequestDto: ContentGenerateRequestDto
     ): String {
+        contentGenerateUseCase.generate(
+            ContentGenerateCommand().apply {
+
+            }
+        )
         return "1"
     }
 }
---
+++ happiness-app-api/src/main/kotlin/com/happiness/api/v1/application/port/in/ContentGenerateUseCase.kt
@@ -0,0 +1,10 @@
+package com.happiness.api.v1.application.port.`in`
+
+import com.happiness.api.v1.application.port.command.ContentGenerateCommand
+import org.springframework.stereotype.Service
+
+// @TODO 에러를 잠시 제거하기 위하여 추가해둔 상태
+@Service
+interface ContentGenerateUseCase {
+    fun generate(command: ContentGenerateCommand): String
+}
