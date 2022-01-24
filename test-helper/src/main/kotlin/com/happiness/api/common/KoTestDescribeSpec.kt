50b7ede05405c53df0572ca6620f86dc25795322
14km <k.connor614@gmail.com>
2022-01-24 23:57:26 +0900

    KoTestDescribeSpec.kt 추가

--- test-helper/src/main/kotlin/com/happiness/api/common/KoTestDescribeSpec.kt
+++ test-helper/src/main/kotlin/com/happiness/api/common/KoTestDescribeSpec.kt
@@ -1,10 +1,23 @@
 package com.happiness.api.common
 
+import com.happiness.api.config.MockMvcTestConfig
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.extensions.spring.SpringExtension
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.MockMvc
 
-abstract class KoTestDescribeSpec(body: DescribeSpec.() -> Unit = {}) {
+@AutoConfigureMockMvc
+@Import(
+    MockMvcTestConfig::class
+)
+@WebAppConfiguration
+abstract class KoTestDescribeSpec(body: DescribeSpec.() -> Unit = {}) : DescribeSpec(body) {
 
-    init {
-        body
-    }
+    override fun extensions() = listOf(SpringExtension)
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
 }
