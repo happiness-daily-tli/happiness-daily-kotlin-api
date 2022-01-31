7f5e0b7b0d5d004792e3da871de86c3c7d2edc78
14km <k.connor614@gmail.com>
2022-02-01 00:13:01 +0900

    ContentGenerateAdapter, ContentGeneratePort Setting

--- happiness-app-api/src/main/kotlin/com/happiness/api/v1/content/application/service/ContentGenerateService.kt
+++ happiness-app-api/src/main/kotlin/com/happiness/api/v1/content/application/service/ContentGenerateService.kt
@@ -3,9 +3,10 @@ package com.happiness.api.v1.content.application.service
 import annotation.UseCase
 import com.happiness.api.v1.content.application.port.`in`.ContentGenerateUseCase
 import com.happiness.api.v1.content.application.port.command.ContentGenerateCommand
+import com.happiness.api.v1.content.application.port.out.ContentGeneratePort
 
 @UseCase
-class ContentGenerateService : ContentGenerateUseCase {
+class ContentGenerateService() : ContentGenerateUseCase {
 
     override fun generate(command: ContentGenerateCommand): String {
         TODO("Not yet implemented")
