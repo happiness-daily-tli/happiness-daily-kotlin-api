d4e5da89ee5eaf6b1be5b3c79ec3d68c3b65dd14
14km <k.connor614@gmail.com>
2022-02-01 00:27:08 +0900

    예시 파일 커밋

--- happiness-app-api/src/test/kotlin/com/happiness/api/v1/content/adapter/in/presentation/ContentQueryControllerTest.kt
+++ happiness-app-api/src/test/kotlin/com/happiness/api/v1/content/adapter/in/presentation/ContentQueryControllerTest.kt
@@ -1,7 +1,27 @@
 package com.happiness.api.v1.content.adapter.`in`.presentation
 
 import io.kotest.core.spec.DisplayName
+import io.kotest.core.spec.style.DescribeSpec
 
 @DisplayName("ContentGenerateController 클래스")
-class ContentQueryControllerTest {
-}
+class ContentQueryControllerTest : DescribeSpec({
+    describe("score") {
+        it("start as zero") {
+            // test here
+        }
+        describe("with a strike") {
+            it("adds ten") {
+                // test here
+            }
+            it("carries strike to the next frame") {
+                // test here
+            }
+        }
+
+        describe("for the opposite team") {
+            it("Should negate one score") {
+                // test here
+            }
+        }
+    }
+})
