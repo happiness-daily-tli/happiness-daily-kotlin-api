14856f81b415b6556cbe14355b120b8bde7cf083
14km <k.connor614@gmail.com>
2022-02-08 23:51:21 +0900

    Content App, Adapter 추가

--- happiness-app-api/src/main/kotlin/com/happiness/api/v1/content/adapter/out/infrastructure/persistence/ContentGenerateAdapter.kt
+++ happiness-app-api/src/main/kotlin/com/happiness/api/v1/content/adapter/out/infrastructure/persistence/ContentGenerateAdapter.kt
@@ -1,10 +1,21 @@
 package com.happiness.api.v1.content.adapter.out.infrastructure.persistence
 
+import com.happiness.api.v1.content.application.port.command.ContentGenerateCommand
 import com.happiness.api.v1.content.application.port.out.ContentGeneratePort
+import com.happiness.api.v1.content.application.port.out.FindContentPort
+import domain.content.Content
+import org.springframework.stereotype.Component
 
-class ContentGenerateAdapter: ContentGeneratePort {
+@Component
+class ContentGenerateAdapter(
+    private val contentRepository: ContentRepository
+) : ContentGeneratePort, FindContentPort {
 
-    override fun saveContent(): String {
+    override fun save(content: Content) {
+        contentRepository.save(content)
+    }
+
+    override fun find(): Content {
         TODO("Not yet implemented")
     }
 }
--- happiness-app-api/src/main/kotlin/com/happiness/api/v1/content/application/port/in/ContentGenerateUseCase.kt
+++ happiness-app-api/src/main/kotlin/com/happiness/api/v1/content/application/port/in/ContentGenerateUseCase.kt
@@ -3,5 +3,5 @@ package com.happiness.api.v1.content.application.port.`in`
 import com.happiness.api.v1.content.application.port.command.ContentGenerateCommand
 
 interface ContentGenerateUseCase {
-    fun generate(command: ContentGenerateCommand): String
+    fun generate(command: ContentGenerateCommand)
 }
