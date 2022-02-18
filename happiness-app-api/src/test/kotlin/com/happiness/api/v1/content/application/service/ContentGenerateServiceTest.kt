dc948784df44af5c6939aeeb0860cd18e359f7ed
14km <k.connor614@gmail.com>
2022-02-18 22:47:28 +0900

    ContentGenerateServiceTest.kt 추가

---
+++ happiness-app-api/src/test/kotlin/com/happiness/api/v1/content/application/service/ContentGenerateServiceTest.kt
@@ -0,0 +1,24 @@
+package com.happiness.api.v1.content.application.service
+
+import com.happiness.api.common.KoTestDescribeSpec
+
+class ContentGenerateServiceTest : KoTestDescribeSpec() {
+
+    /**
+     * Describe	설명할 테스트 대상을 명시한다.
+     * Context	테스트 대상이 놓인 상황을 설명한다.
+     * It	테스트 대상의 행동을 설명한다.
+     **/
+    init {
+        describe("Content Generate Service") {
+            context("올바른 컨텐츠 데이터를 전달 받은 경우") {
+
+            }
+
+            it("데이터 저장에 성공한다.") {
+
+            }
+        }
+
+    }
+}
