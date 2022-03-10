8b38004d982a44e523b3e3f1dba93ec137c62811
14km <k.connor614@gmail.com>
2022-03-11 02:04:46 +0900

    당장 Transactional이 필요한 부분이 없으므로 제거합니다.

--- happiness-app-api/src/main/kotlin/com/happiness/api/v1/content/adapter/out/infrastructure/persistence/ContentGenerateAdapter.kt
+++ happiness-app-api/src/main/kotlin/com/happiness/api/v1/content/adapter/out/infrastructure/persistence/ContentGenerateAdapter.kt
@@ -1,14 +1,11 @@
 package com.happiness.api.v1.content.adapter.out.infrastructure.persistence
 
-import com.happiness.api.v1.content.application.port.command.ContentGenerateCommand
 import com.happiness.api.v1.content.application.port.out.ContentGeneratePort
 import com.happiness.api.v1.content.application.port.out.FindContentPort
 import domain.content.Content
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Transactional
 
 @Component
-@Transactional
 class ContentGenerateAdapter(
     private val contentRepository: ContentRepository
 ) : ContentGeneratePort, FindContentPort {
@@ -17,7 +14,6 @@ class ContentGenerateAdapter(
         contentRepository.save(content)
     }
 
-    @Transactional(readOnly = true)
     override fun find(contentCode: String): Content {
         TODO("Content Return")
 //        return contentRepository.findByContentCode(contentCode)
