04e354dcc5d1987a1024413280b63b7e19a079e8
14km <k.connor614@gmail.com>
2022-03-11 22:32:59 +0900

    gradle 설정 변경

--- happiness-app-api/build.gradle.kts
+++ happiness-app-api/build.gradle.kts
@@ -12,12 +12,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     testImplementation(project(":test-helper"))
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
-    testImplementation("com.ninja-squad:springmockk:3.0.1")
-}
-
-springBoot {
-    buildInfo()
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
 }
